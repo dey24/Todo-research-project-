8f346dd6d439538d415269f63273b8c6e4d03d6b
Chiranjit Dey <chiranjitdey2000@gmail.com>
2020-10-05 20:49:24 +0530

    check and delete todo

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //selector
 document.querySelector('form').addEventListener('submit', handleSubmitForm);
+document.querySelector('ul').addEventListener('click', handleClickDeleteOrCheck);
 
 //Event handler
 function handleSubmitForm(e){
@@ -8,6 +9,14 @@ function handleSubmitForm(e){
     if(input.value != '')
         addTodo(input.value);
     input.value = '';
+}
+
+function handleClickDeleteorCheck(e){
+    if(e.target.name == 'checkButton')
+    checkTodo(e);
+
+    if(e.target.name == 'deleteButton')
+    deleteTodo(e);
 }
 
 
@@ -26,4 +35,21 @@ function addTodo(todo){
 
     li.classList.add('todo-list-item');
     ul.appendChild(li);
+}
+
+function checkTodo(e){
+    let item = e.target.parentNode;
+    if(item.style.textDecoration == 'line-through')
+        item.style.textDecoration = 'none';
+    else
+        item.style.textDecoration = 'line-through';
+}
+
+function deleteTodo(e){
+    let item = e.target.parentNode;
+    item.addEventListener('transitionend', function(){
+        item.remove();
+    });
+
+    item.classList.add('todo-list-item-fall');
 }
